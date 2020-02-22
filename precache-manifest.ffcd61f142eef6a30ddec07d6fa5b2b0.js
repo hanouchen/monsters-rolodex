c6676bdd89bfa3f1fc78edebb4b02f7e5e59902e
Hanou Chen <chenhanou13@gmail.com>
2020-02-22 14:40:21 +0000

    Updates

---
+++ precache-manifest.ffcd61f142eef6a30ddec07d6fa5b2b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e998d5b44953c4d0c0b92cdc84becd6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0a6d9e27c9ec6f1de66e",
+    "url": "/monsters-rolodex/static/css/main.6096c219.chunk.css"
+  },
+  {
+    "revision": "069939a6b77131a9320a",
+    "url": "/monsters-rolodex/static/js/2.1cdc3a1a.chunk.js"
+  },
+  {
+    "revision": "0a6d9e27c9ec6f1de66e",
+    "url": "/monsters-rolodex/static/js/main.55819149.chunk.js"
+  },
+  {
+    "revision": "d17e922d3486072ea697",
+    "url": "/monsters-rolodex/static/js/runtime-main.a3da273f.js"
+  }
+]);
