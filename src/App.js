0317ce377f960db7006c20f0b88820c16dd1b1ab
Hanou Chen <chenhanou13@gmail.com>
2020-02-22 14:47:16 +0000

    Use cors

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class MyApp extends Component {
   }
 
   componentDidMount () {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users', {mode: 'cors'})
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }))
       .catch(error => { console.log(error); });
